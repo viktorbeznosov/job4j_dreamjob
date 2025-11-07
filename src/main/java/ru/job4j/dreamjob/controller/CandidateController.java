41386d79f867f3e8b22a1ba51a36e35cc9000ade
viktorbeznosov <viktorbeznosov@mail.ru>
2025-11-07 16:48:01 +0300

    9. HTML form. Создание страницы кандидата [#504845]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -19,4 +19,9 @@ public class CandidateController {
         model.addAttribute("candidates", candidateRepository.findAll());
         return "candidates/list";
     }
+
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "candidates/create";
+    }
 }
