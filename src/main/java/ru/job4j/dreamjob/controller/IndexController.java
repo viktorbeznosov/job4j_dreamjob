605659008875014583e2b2932b5dcefe2d2ca20d
viktorbeznosov <viktorbeznosov@mail.ru>
2025-11-19 22:39:06 +0300

    Логирование [#505129 #555151]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,13 +1,18 @@
 package ru.job4j.dreamjob.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 @SuppressWarnings("unused")
 public class IndexController {
     @GetMapping("/index")
     public String getIndex() {
+        log.error("Some error occured");
+        log.warn("Some warn occured");
+        log.info("Some info occured");
         return "index";
     }
 }
